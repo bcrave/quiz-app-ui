82562125c46e3e44f6a39c2a19a39a3ef7f78663
Brendon Crave <brennycode@gmail.com>
2021-09-22 13:14:25 +0400

    removed unnecessary code from js file

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 let counter = 0;
 
-// Question Counters
-const questionCounter1 = document.getElementById("question-counter1");
-const questionCounter2 = document.getElementById("question-counter2");
-const questionCounter3 = document.getElementById("question-counter3");
 const questionCounters = document.querySelectorAll(".question-counter");
-
-// Questions
-const question1 = document.getElementById("question1");
-const question2 = document.getElementById("question2");
-const question3 = document.getElementById("question3");
 const questions = document.querySelectorAll(".question");
-
-// Answers
-const answersGroup1 = document.getElementById("answers1");
-const answersGroup2 = document.getElementById("answers2");
-const answersGroup3 = document.getElementById("answers3");
 const answersGroups = document.querySelectorAll(".answers");
 
 const button = document.querySelector("button");
